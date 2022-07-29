197df15d039764763e9e6c2fa07ee7e4b036018b
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-29 20:44:34 +0200

    Protected modifier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   //   private readonly id: string;
   //   private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -34,6 +34,13 @@ class AccountingDepartment extends Department {
     super(id, "Accounting");
   }
 
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -58,7 +65,11 @@ console.log(it);
 const accounting = new AccountingDepartment("d2", []);
 accounting.addReport("Something went wrong!");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: "Tomek", describe: accounting.describe };
 
