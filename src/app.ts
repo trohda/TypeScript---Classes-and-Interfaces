9d6cab478949a6d31ee49706f32c8242de590a2b
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-29 19:16:11 +0200

    Using "this"

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,16 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
 const accounting = new Department("Accounting");
 
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: "Tomek", describe: accounting.describe };
+
+accountingCopy.describe();
