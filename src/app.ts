5662ac121cfa7ecd6a90e6745d2de1dfa4d7e97f
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-29 19:27:42 +0200

    Access (private)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,27 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
+accounting.addEmployee("Tomek");
+accounting.addEmployee("Gosia");
+
+// accounting.employees[2] = "Max";
 
 accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: "Tomek", describe: accounting.describe };
+// const accountingCopy = { name: "Tomek", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
